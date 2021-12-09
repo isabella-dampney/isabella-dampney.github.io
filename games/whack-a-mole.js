9ee1aef90af30668fe3f74047eeaac4f51d8bd85
Isabella-Dampney <isabella.dampney@gmail.com>
2021-12-09 18:20:20 +1300

    working on it

--- games/whack-a-mole.js
+++ games/whack-a-mole.js
@@ -8,7 +8,7 @@ var randomIndex = Math.floor(Math.random() * cells.length); // creates a random 
 var randomCell = cells[randomIndex] // then we assign that number to a cell to select a random cell. 
 
 var mole = document.createElement('img');   // creates HTML element, and tells it that it's a img. It is accessable through "mole". 
-mole.src = './mole.PNG';
+mole.src = './games/mole.PNG';
 mole.id = "mole";                       //links to the CSS to style img to fit cell
 randomCell.appendChild(mole);           // the appendChild adds the variable mole which is the image to the variable randomCell. This way they operate together. 
 //parentNode.appendChild(childNode);    The appendChild() returns the appended child.
