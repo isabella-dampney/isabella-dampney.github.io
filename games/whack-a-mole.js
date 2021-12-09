e9ae8543f01686312e73d03628b2cbbc52c1337d
Isabella-Dampney <isabella.dampney@gmail.com>
2021-12-09 18:16:15 +1300

    attempt for images to load

--- games/whack-a-mole.js
+++ games/whack-a-mole.js
@@ -7,10 +7,10 @@ var randomIndex = Math.floor(Math.random() * cells.length); // creates a random 
 
 var randomCell = cells[randomIndex] // then we assign that number to a cell to select a random cell. 
 
-var mole = document.createElement('img');   // creates HTML element, and tells it that it's a img. It is accessable through "mole". 
-mole.src = 'fart.PNG';
-mole.id = "mole";                       //links to the CSS to style img to fit cell
-randomCell.appendChild(mole);           // the appendChild adds the variable mole which is the image to the variable randomCell. This way they operate together. 
+var fart = document.createElement('img');   // creates HTML element, and tells it that it's a img. It is accessable through "mole". 
+fart.src = 'fart.PNG';
+fart.id = "fart";                       //links to the CSS to style img to fit cell
+randomCell.appendChild(fart);           // the appendChild adds the variable mole which is the image to the variable randomCell. This way they operate together. 
 //parentNode.appendChild(childNode);    The appendChild() returns the appended child.
 
 mole.onclick = whackedMole              // When you click on the mole 
@@ -18,7 +18,7 @@ mole.onclick = whackedMole              // When you click on the mole
 function whackedMole () {
   randomIndex = Math.floor(Math.random() * cells.length);
   randomCell = cells[randomIndex];
-  randomCell.appendChild(mole); 
+  randomCell.appendChild(fart); 
 
   var audio = new Audio();
   audio.src = "fart.wav";
